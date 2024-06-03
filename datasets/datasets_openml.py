2f1bdcb91ee1024833262293ca392fc5517cdeb7
SebastienMelo <sebastien.melo@polytechnique.edu>
2024-06-03 12:00:10 +0200

    first solvers

--- datasets/datasets_openml.py
+++ datasets/datasets_openml.py
@@ -5,29 +5,7 @@ with safe_import_context() as import_ctx:
 
 
 DATASETS = {
-    'bank-marketing': 44126,
-    'california': 44090,
-    'compass': 44162,
-    'covertype': 44121,
-    'covertype_categorical': 44159,
-    'elevators': 45031,
-    'credit': 44089,
-    'electricity': 44120,
-    'electricity_categorical': 44156,
-    'eyemovements': 44130,
-    'eyemovements_categorical': 44156,
-    'higgs': 44129,
-    'house_16H': 44123,
-    'jannis': 44131,
-    'KDDCup06_upselling': 44186,
-    'kdd_ipums_la_97-small': 44124,
-    'magictelescope': 44125,
-    'MiniBooNE': 44128,
-    'phoneme': 44127,
-    'pol': 44122,
-    'rl': 44160,
-    'road_safety': 44161,
-    'wine': 44091,
+    'elevators': 45031
 }
 
 
